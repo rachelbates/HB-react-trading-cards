4ab0403eb9d8e36ec3e3de923baa6df3b66e56e3
Rachel Bates <rachelbatesnj@gmail.com>
2021-05-20 21:54:00 +0000

    Finished Assignment

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,74 +1,95 @@
 "use strict";
 const tradingCardData = [
-  //add the rest of the trading card properties(??) for each animal into this list as dictionries
   {
     name: 'Balloonicorn',
     skill: 'video games',
     imgUrl: '/static/img/balloonicorn.jpg'
-  }
-]
+  },
 
-function TradingCard(props) {
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpeg'
+  },
+
+  {
+    name: 'Seed.py',
+    skill: 'making curry dishes',
+    imgUrl: '/static/img/seedpy.jpeg'
+  },
+
+  {
+    name: 'Polymorphism',
+    skill: 'costumes',
+    imgUrl: '/static/img/polymorphism.jpeg'
+  },
+
+  {
+    name: 'Short Stack Overflow',
+    skill: 'ocean animal trivia',
+    imgUrl: '/static/img/shortstack-overflow.jpeg'
+  },
+
+  {
+    name: 'Merge',
+    skill: 'bullet journaling',
+    imgUrl: '/static/img/merge.png'
+  }
+];
+
+// define TradingCard component
+   // render one trading card
+
+  function TradingCard(props) {
+    return (
+      <div className="card">
+        <h2>Name: {props.name}</h2>
+        <img src={props.imgUrl} />
+        <h2>Skill: {props.skill}</h2>
+      </div>
+    );
+  }
+
+// define TradingCardContainer component
+function TradingCardContainer() {
+  //create empty list called paragraphs
+  const tradingCards = [];
+
+  //looping through each entry in tradingCardData dict
+  //...and pushing name, skill, and img into empty list tradingCards
+  for (const currentCard of tradingCardData) {
+    tradingCards.push(
+      <TradingCard
+        name={currentCard.name}
+        skill={currentCard.skill}
+        imgUrl={currentCard.imgUrl}
+      />
+    );
+  }
+  //turning paragraph list of all names within a <React.Fragment> 
   return (
-    <div className="card">
-      <h2>Name: {props.name}</h2>
-      <img src={props.imgUrl} />
-      <h2>Skill: {props.skill}</h2>
-    </div>
+    <React.Fragment>
+      {tradingCards}
+    </React.Fragment>
   );
 }
 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Balloonicorn"
-      skill="video games"
-      imgUrl="/static/img/balloonicorn.jpg"
-    />
-  ),
-  document.querySelector('#balloonicorn')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Float"
-      skill="baking pretzels"
-      imgUrl="/static/img/float.jpg"
-    />
-  ),
-  document.querySelector('#float')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Llambda"
-      skill="knitting scarves"
-      imgUrl="/static/img/llambda.jpg"
-    />
-  ),
-  document.querySelector('#llambda')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Merge"
-      skill="swimming fast"
-      imgUrl="/static/img/merge.png/"
-    />
-  ),
-  document.querySelector('#merge')
-);
- 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Polymorphism"
-      skill="purring"
-      imgUrl="/static/img/polymorphism.jpeg/"
-    />
-  ),
-  document.querySelector('#polymorphism')
+// mount just ONE thing onto the page: (ReactDOM.render)
+// one TradingCardContainer component
+ReactDOM.render( 
+  <TradingCardContainer/>,
+  document.querySelector('.grid')
 );
